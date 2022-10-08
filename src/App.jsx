726a011865c86572fc489c367db2a9d9f1ee4acc
StrigZ <mirgasan2010@ya.ru>
2022-10-08 16:54:37 +0400

    add Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import "./App.css";
 import Chat from "./Chat";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Header />
-      <AppBody>
-        <Sidebar />
-        <Chat />
-        <Outlet />
-      </AppBody>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Chat />
+            <Outlet />
+          </AppBody>
+        </>
+      )}
     </div>
   );
 }
